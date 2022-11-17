53065c61a091ea444b64f9ccd5d95c5b6f7bdb6b
KEREM BAS <kerembas2000@gmail.com>
2022-11-17 16:10:37 +0300

    main

---
+++ lib/config/ThemeColor.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/animation.dart';
+
+Color color1 = Color.fromARGB(255, 59, 52, 134);
+Color color2 = Color.fromARGB(255, 119, 67, 219);
+Color color3 = Color.fromARGB(255, 255, 233, 177);
+Color color4 = Color.fromARGB(255, 252, 253, 242);
